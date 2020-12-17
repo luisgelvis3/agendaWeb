217a9292b04ea72947a7ec2c6afb502d1fa8234c
Luis Gelvis <luisgelvis123@gmail.com>
2020-12-17 13:26:47 -0500

    inicio de clases

---
+++ Model/clases/persona.php
@@ -0,0 +1,29 @@
+<?php
+
+class Persona{
+    //Atributos
+    public $nombre;
+    public $documento;
+    public $direccion;
+    public $telefono;
+    public $correo;
+
+    //Constructor
+    public function __construct(){
+
+    }
+
+    //Destructor
+    public function __destruct(){
+        
+    }
+
+    //Metodos    
+
+    public function insertUser($nombre, $documento, $direccion, $telefono, $correo){
+        
+    }
+
+}
+
+?>
---
+++ Model/clases/usuario.php
@@ -0,0 +1,24 @@
+<?php
+
+class Usuario{
+    //atributos
+    public $usuario;
+    public $password;
+
+    //Constructor
+    public function __construct(){
+
+    }
+
+    //Destructor
+    public function __destruct(){
+        
+    }
+
+    //metodos
+    public function inicioLogin($usuario, $password){
+        
+    }
+}
+
+?>
